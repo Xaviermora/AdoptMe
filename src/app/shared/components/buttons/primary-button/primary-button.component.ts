aaff581a0c3a58b364ad42d982214eee46292af7
Xaviermora <xaviermoravega12@gmail.com>
2023-07-07 00:19:45 -0300

    Creo el componente primary button

---
+++ src/app/shared/components/buttons/primary-button/primary-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-primary-button',
+  templateUrl: './primary-button.component.html',
+  styleUrls: ['./primary-button.component.css']
+})
+export class PrimaryButtonComponent {
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { PrimaryButtonComponent } from './components/buttons/primary-button/primary-button.component';
 
 @NgModule({
   declarations: [
-    NavbarComponent
+    NavbarComponent,
+    PrimaryButtonComponent
   ],
   exports: [
     NavbarComponent
