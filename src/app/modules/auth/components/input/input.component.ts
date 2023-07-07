88d1e348072f31b42465049732741e29f75a3ea9
Xaviermora <xaviermoravega12@gmail.com>
2023-07-06 22:58:16 -0300

    Correción id del input component

--- src/app/modules/auth/components/input/input.component.ts
+++ src/app/modules/auth/components/input/input.component.ts
@@ -8,5 +8,5 @@ import { Component, Input } from '@angular/core';
 export class InputComponent {
   @Input() type!: string
   @Input() label!: string
-  @Input() id!: string
+  @Input() idInput!: string
 }
