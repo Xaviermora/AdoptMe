0f5c055912013d9ad2d6c59cb9300a6adfc1e419
Xaviermora <xaviermoravega12@gmail.com>
2023-07-06 23:14:42 -0300

    Cambio de nombre del componente boton auth google a button auth google

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -5,7 +5,7 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { InputComponent } from './components/input/input.component';
-import { BotonAuthGoogleComponent } from './components/boton-auth-google/boton-auth-google.component';
+import { ButtonAuthGoogleComponent } from './components/button-auth-google/button-auth-google.component';
 
 
 @NgModule({
@@ -13,7 +13,7 @@ import { BotonAuthGoogleComponent } from './components/boton-auth-google/boton-a
     LoginComponent,
     RegisterComponent,
     InputComponent,
-    BotonAuthGoogleComponent
+    ButtonAuthGoogleComponent
   ],
   imports: [
     CommonModule,
