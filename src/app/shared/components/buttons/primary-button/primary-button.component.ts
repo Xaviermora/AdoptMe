7de7db501be2f887e554b7a0921c3668de7509d6
Xaviermora <xaviermoravega12@gmail.com>
2023-07-07 00:26:25 -0300

    Añado funcionalidad de pasar el contenido a mostrar

--- src/app/shared/components/buttons/primary-button/primary-button.component.ts
+++ src/app/shared/components/buttons/primary-button/primary-button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-primary-button',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./primary-button.component.css']
 })
 export class PrimaryButtonComponent {
-
+  @Input() contenido!: string
 }
