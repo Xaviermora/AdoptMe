675642a91ed4bdf7156d1260daa51799851a53d9
GonzaloLatorre <89478526+GonzaloLatorre@users.noreply.github.com>
2023-07-12 12:35:47 -0300

    listo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: '',
+  {path: 'animales',
   loadChildren:()=>import('./modules/animales/animales.module').then( m => m.AnimalesModule)
 }
 ];
