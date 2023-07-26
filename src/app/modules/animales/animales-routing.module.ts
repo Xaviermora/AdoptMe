87d24f80c4adde040afdb0fe9d438895dbdc5eab
GonzaloLatorre <89478526+GonzaloLatorre@users.noreply.github.com>
2023-07-26 19:01:56 -0300

    defini la ruta que va a mostrar la card

--- src/app/modules/animales/animales-routing.module.ts
+++ src/app/modules/animales/animales-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CardComponent } from './components/card/card.component';
 
 const routes: Routes = [
+  {
+    path: '' ,
+    component: CardComponent
+  }
 ];
 
 @NgModule({
