286cbf57bb00278c13c417cf73011e53cacb7848
GonzaloLatorre <89478526+GonzaloLatorre@users.noreply.github.com>
2023-07-26 19:56:32 -0300

    agrege los estilos a la card

--- src/app/modules/animales/animales.module.ts
+++ src/app/modules/animales/animales.module.ts
@@ -4,16 +4,18 @@ import { CommonModule } from '@angular/common';
 import { AnimalesRoutingModule } from './animales-routing.module';
 import { CardComponent } from './components/card/card.component';
 import { ModalComponent } from './components/card/modal/modal.component';
+import { SharedModule } from "../../shared/shared.module";
 
 
 @NgModule({
-  declarations: [
-    CardComponent,
-    ModalComponent
-  ],
-  imports: [
-    CommonModule,
-    AnimalesRoutingModule
-  ]
+    declarations: [
+        CardComponent,
+        ModalComponent
+    ],
+    imports: [
+        CommonModule,
+        AnimalesRoutingModule,
+        SharedModule
+    ]
 })
 export class AnimalesModule { }
