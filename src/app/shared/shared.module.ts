bba05e76d8cdc2fa1f4ded3ecf47e6c84c6ef3f6
Xaviermora <xaviermoravega12@gmail.com>
2023-08-09 19:33:26 -0300

    Creación del estructurado, propiedades y estilos del button

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { PrimaryButtonComponent } from './components/buttons/primary-button/primary-button.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    PrimaryButtonComponent
+    PrimaryButtonComponent,
+    ButtonComponent
   ],
   exports: [
     NavbarComponent,
-    PrimaryButtonComponent
+    PrimaryButtonComponent,
+    ButtonComponent
   ],
   imports: [
     CommonModule
