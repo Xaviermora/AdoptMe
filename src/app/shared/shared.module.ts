420571b7322f46d35ed6a1f5d1610f5a705a2076
Xaviermora <xaviermoravega12@gmail.com>
2023-08-09 19:34:36 -0300

    Se elimina anterior componente del primary button

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { PrimaryButtonComponent } from './components/buttons/primary-button/primary-button.component';
 import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    PrimaryButtonComponent,
     ButtonComponent
   ],
   exports: [
     NavbarComponent,
-    PrimaryButtonComponent,
     ButtonComponent
   ],
   imports: [
