7903b16ae95e9b50b94f97752ea0ff36273f8b4a
Xaviermora <xaviermoravega12@gmail.com>
2023-08-09 21:20:32 -0300

    Estructurado y estilos del divider

---
+++ src/app/shared/components/divider/divider.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-divider',
+  template: '<div class="h-px w-full"></div>',
+  styles: ['div {background-color: var(--neutral-secundario-50);}']
+})
+
+export class DividerComponent {
+
+}
