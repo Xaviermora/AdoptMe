fa8029f79f1a6549d6ad539a5be14eb07a3ccb2a
Xaviermora <xaviermoravega12@gmail.com>
2023-08-14 23:28:23 -0300

    Correción estilo neutral secundario 50

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ module.exports = {
         primario: '#2E2E2E',
         secundario: {
           100: '#4D4D4D',
-          50: '#4D4D4D'
+          50: '#4d4d4d80'
         }
       },
       hover: '#F5F4FB'
