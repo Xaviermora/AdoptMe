bd1c859e97c0e81c62dac4fe797589d1cd152afd
Xaviermora <xaviermoravega12@gmail.com>
2023-08-18 17:41:04 -0300

    Se añade funcionalidad de cambiar las imagenes

--- src/app/modules/animales/components/card/card.component.ts
+++ src/app/modules/animales/components/card/card.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Carousel, CarouselItem } from 'flowbite';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent {
+export class CardComponent implements OnInit{  
+  imagenesAnimal: string[] = [
+    '../../../../../assets/michi.jpeg',
+    '../../../../../assets/michi.jpeg'
+  ]
 
+  constructor(){}
+
+  ngOnInit(){
+    const carousel = new Carousel();
+    const $prevButton = document.getElementById('data-carousel-prev');
+    const $nextButton = document.getElementById('data-carousel-next');
+
+    $prevButton?.addEventListener('click', () => carousel.prev())
+
+    $nextButton?.addEventListener('click', () => carousel.next())
+  }
 }
