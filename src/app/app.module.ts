dcb459a2d3990bcd9b2821362f79ef26d521ea0f
brashian <bsandovala12@gmail.com>
2023-08-18 02:10:24 -0300

    Instalacion firebase/dependencia y importacion de modulos de configuracion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,11 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { SharedModule } from './shared/shared.module';
 
+//FIREBASE
+import { environment } from 'src/enviroments/enviroment.prod';
+import {AngularFireModule} from '@angular/fire/compat'
+import {AngularFireAuthModule} from '@angular/fire/compat/auth'
+import {AngularFireStorageModule} from '@angular/fire/compat/storage'
 @NgModule({
   declarations: [
     AppComponent
