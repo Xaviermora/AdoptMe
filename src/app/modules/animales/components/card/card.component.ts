d432ff9c448ed605f24df2126e3dd1abb33b97e9
Xaviermora <xaviermoravega12@gmail.com>
2023-08-20 12:24:24 -0300

    Funcionalidad del movimiento de imagenes

--- src/app/modules/animales/components/card/card.component.ts
+++ src/app/modules/animales/components/card/card.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Carousel, CarouselItem } from 'flowbite';
+import { Component, OnInit } from '@angular/core';
+import { Carousel, CarouselOptions, IndicatorItem } from 'flowbite';
 
 @Component({
   selector: 'app-card',
@@ -9,18 +9,22 @@ import { Carousel, CarouselItem } from 'flowbite';
 export class CardComponent implements OnInit{  
   imagenesAnimal: string[] = [
     '../../../../../assets/michi.jpeg',
-    '../../../../../assets/michi.jpeg'
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Cat_on_its_back.jpg/220px-Cat_on_its_back.jpg'
   ]
 
   constructor(){}
-
+  
   ngOnInit(){
     const carousel = new Carousel();
+
+    // Funcionalidad del movimiento de imagenes siguiente y anterior
     const $prevButton = document.getElementById('data-carousel-prev');
     const $nextButton = document.getElementById('data-carousel-next');
-
+  
     $prevButton?.addEventListener('click', () => carousel.prev())
-
+  
     $nextButton?.addEventListener('click', () => carousel.next())
   }
+
+  x(){console.log('d')}
 }
