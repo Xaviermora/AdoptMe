b0f9908575207ea70e36a448fc5877c1fe6e1763
Xaviermora <xaviermoravega12@gmail.com>
2023-08-21 01:23:16 -0300

    Creación de carousel personalizado

--- src/app/modules/animales/components/card/card.component.ts
+++ src/app/modules/animales/components/card/card.component.ts
@@ -1,30 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { Carousel, CarouselOptions, IndicatorItem } from 'flowbite';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent implements OnInit{  
+export class CardComponent{  
   imagenesAnimal: string[] = [
     '../../../../../assets/michi.jpeg',
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Cat_on_its_back.jpg/220px-Cat_on_its_back.jpg'
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Cat_on_its_back.jpg/220px-Cat_on_its_back.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Gato_Siam%C3%A9s_ojos_azules.JPG/220px-Gato_Siam%C3%A9s_ojos_azules.JPG'
   ]
+  imgActual: number = 0 // Posición de la imagen que se esta mostrando en el carousel
 
-  constructor(){}
-  
-  ngOnInit(){
-    const carousel = new Carousel();
-
-    // Funcionalidad del movimiento de imagenes siguiente y anterior
-    const $prevButton = document.getElementById('data-carousel-prev');
-    const $nextButton = document.getElementById('data-carousel-next');
-  
-    $prevButton?.addEventListener('click', () => carousel.prev())
-  
-    $nextButton?.addEventListener('click', () => carousel.next())
+  constructor(){
   }
 
-  x(){console.log('d')}
+  anteriorImg(){
+    this.imgActual -= 1
+
+    this.imagenesAnimal.forEach((_,i) => document.getElementById(`img-${i}`)!.style.transform = `translateX(-${(this.imgActual)*100}%)`)
+  }
+
+  siguienteImg(){
+    this.imgActual += 1
+
+    this.imagenesAnimal.forEach((_,i) => document.getElementById(`img-${i}`)!.style.transform = `translateX(-${(this.imgActual)*100}%)`)
+  }
 }
