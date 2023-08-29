f7ddc4fa4972901cb4af783bd75af03053ea79cf
brashian <bsandovala12@gmail.com>
2023-08-29 10:55:01 -0300

    creacion del componente footer

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,17 +3,20 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ButtonComponent } from './components/button/button.component';
 import { DividerComponent } from './components/divider/divider.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     NavbarComponent,
     ButtonComponent,
-    DividerComponent
+    DividerComponent,
+    FooterComponent
   ],
   exports: [
     NavbarComponent,
     ButtonComponent,
-    DividerComponent
+    DividerComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
