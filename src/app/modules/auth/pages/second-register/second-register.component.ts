4ba630a4bc5a8450ba9619e58580babc39f1d86e
Xaviermora <xaviermoravega12@gmail.com>
2023-09-01 22:05:57 -0300

    Se crea el componente second register

---
+++ src/app/modules/auth/pages/second-register/second-register.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-second-register',
+  templateUrl: './second-register.component.html',
+  styleUrls: ['./second-register.component.css']
+})
+export class SecondRegisterComponent {
+
+}
