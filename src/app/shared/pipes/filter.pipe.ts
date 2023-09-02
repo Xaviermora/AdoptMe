fae7b3df391f9487cdabeabd30ed4c0803a36551
Xaviermora <xaviermoravega12@gmail.com>
2023-09-02 19:00:46 -0300

    Logica de la pipe

--- src/app/shared/pipes/filter.pipe.ts
+++ src/app/shared/pipes/filter.pipe.ts
@@ -3,10 +3,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({
   name: 'filter'
 })
+
 export class FilterPipe implements PipeTransform {
-
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(list: string[], searchValue: string = ''): string[]{
+    return list.map(item => item.toLowerCase()) // Devuelve un nuevo array con todos los items en minúscula
+              .filter(item => item.includes(searchValue.toLowerCase())) // Devuelve los items que contengan algun valor de la busqueda
   }
-
 }
