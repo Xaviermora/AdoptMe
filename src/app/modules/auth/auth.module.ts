6fc6301d24c0fb19b9d33665694a343a04cd047d
Xaviermora <xaviermoravega12@gmail.com>
2023-09-01 22:05:36 -0300

    Se crea el componente first register

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -3,19 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './pages/login/login.component';
-import { RegisterComponent } from './pages/register/register.component';
 import { InputComponent } from './components/input/input.component';
 import { ButtonAuthGoogleComponent } from './components/button-auth-google/button-auth-google.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { InputPasswordComponent } from './components/input-password/input-password.component';
+import { FirstRegisterComponent } from './pages/first-register/first-register.component';
 
 @NgModule({
   declarations: [
     LoginComponent,
-    RegisterComponent,
     InputComponent,
     ButtonAuthGoogleComponent,
     InputPasswordComponent,
+    FirstRegisterComponent
   ],
   imports: [
     CommonModule,
