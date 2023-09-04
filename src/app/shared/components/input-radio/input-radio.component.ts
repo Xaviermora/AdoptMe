84e90081c0a683b9fe850f9eec758c1fb7c90462
Xaviermora <xaviermoravega12@gmail.com>
2023-09-04 09:02:30 -0300

    Estructurado y atributos del input radio

--- src/app/shared/components/input-radio/input-radio.component.ts
+++ src/app/shared/components/input-radio/input-radio.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-input-radio',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./input-radio.component.css']
 })
 export class InputRadioComponent {
-
+  @Input() valueInput!: string
+  @Input() name!: string
 }
