19dea2ed44bbcf6d1dcf577b87882d32b4341980
Xaviermora <xaviermoravega12@gmail.com>
2023-09-05 10:30:29 -0300

    Cambio del input a components shared

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './pages/login/login.component';
-import { InputComponent } from './components/input/input.component';
+import { InputComponent } from '../../shared/components/input/input.component';
 import { ButtonAuthGoogleComponent } from './components/button-auth-google/button-auth-google.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { InputPasswordComponent } from './components/input-password/input-password.component';
