2874f286a6a75bf1c191e92caf3554181df694f7
Xaviermora <xaviermoravega12@gmail.com>
2023-09-06 09:29:43 -0300

    Estructurado y atributos del input checkbox

--- src/app/shared/components/input-checkbox/input-checkbox.component.ts
+++ src/app/shared/components/input-checkbox/input-checkbox.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-input-checkbox',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./input-checkbox.component.css']
 })
 export class InputCheckboxComponent {
-
+  @Input() idInput!: string
 }
