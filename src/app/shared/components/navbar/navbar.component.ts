0b9f8b7f36acd376fb3ed1cd59dc786d5acc7636
brashian <bsandovala12@gmail.com>
2023-09-12 09:40:15 -0300

    cambios navbar

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-
+ 
 }
