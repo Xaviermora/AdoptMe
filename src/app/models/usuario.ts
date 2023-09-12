7d68d8fe8d0f52877830b6acaa4201776a37d8e6
Xaviermora <xaviermoravega12@gmail.com>
2023-09-12 10:30:49 -0300

    Propiedades del modelo de usuario

---
+++ src/app/models/usuario.ts
@@ -0,0 +1,14 @@
+export interface Usuario{
+  uid: string;
+  email: string;
+  password: string;
+  nombre: string;
+  apellido: string;
+  fechaDeNacimiento: Date;
+  provincia: string;
+  tipoDeVivienda: string;
+  calle: string;
+  numero: number;
+  telefono: number;
+  dni: number;
+}
