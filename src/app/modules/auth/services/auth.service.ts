54c6b9137e8055ca670bfd94dcfea80f86b372d2
Xaviermora <xaviermoravega12@gmail.com>
2023-09-16 15:19:57 -0300

    Se importa AngularFireAuth

--- src/app/modules/auth/services/auth.service.ts
+++ src/app/modules/auth/services/auth.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private auth: AngularFireAuth) { }
+
+  
 }
