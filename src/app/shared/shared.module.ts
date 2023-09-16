8444d2db713f307757881a0c2a53801ba59814ea
Xaviermora <xaviermoravega12@gmail.com>
2023-09-16 16:47:43 -0300

    Importación de reactiveFormsModules

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,7 +5,7 @@ import { ButtonComponent } from './components/button/button.component';
 import { DividerComponent } from './components/divider/divider.component';
 import { SearchSelectComponent } from './components/search-select/search-select.component';
 import { FilterPipe } from './pipes/filter.pipe';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { InputRadioComponent } from './components/input-radio/input-radio.component';
 import { InputComponent } from './components/input/input.component';
 import { InputCheckboxComponent } from './components/input-checkbox/input-checkbox.component';
@@ -35,7 +35,8 @@ import { InputPhoneComponent } from './components/input-phone/input-phone.compon
   ],
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class SharedModule { }
