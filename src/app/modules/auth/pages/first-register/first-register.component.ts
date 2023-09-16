9b59e37d40b322bfddef3c68fa042493db780b5a
Xaviermora <xaviermoravega12@gmail.com>
2023-09-16 17:02:57 -0300

    metodo ngSubmit

--- src/app/modules/auth/pages/first-register/first-register.component.ts
+++ src/app/modules/auth/pages/first-register/first-register.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
@@ -7,7 +7,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   templateUrl: './first-register.component.html',
   styleUrls: ['./first-register.component.css']
 })
-export class FirstRegisterComponent implements OnInit {
+export class FirstRegisterComponent {
   register1 = new FormGroup({
     email: new FormControl('', Validators.required),
     password: new FormControl('', Validators.required),
@@ -16,8 +16,7 @@ export class FirstRegisterComponent implements OnInit {
 
   constructor(private authService: AuthService){}
 
-  ngOnInit(){
+  onSubmit(){
     console.log(this.register1)
-    //this.authService.register()
   }
 }
