973343705e12a8fc9bcb7c7f4f73035a689772d1
Xaviermora <xaviermoravega12@gmail.com>
2023-09-16 16:51:56 -0300

    Creación de formulario reactivo

--- src/app/modules/auth/pages/first-register/first-register.component.ts
+++ src/app/modules/auth/pages/first-register/first-register.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-first-register',
   templateUrl: './first-register.component.html',
   styleUrls: ['./first-register.component.css']
 })
-export class FirstRegisterComponent {
+export class FirstRegisterComponent implements OnInit {
+  register1 = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+    repeatPassword: new FormControl('', Validators.required)
+  })
 
+  constructor(private authService: AuthService){}
+
+  ngOnInit(){
+    console.log(this.register1)
+    //this.authService.register()
+  }
 }
