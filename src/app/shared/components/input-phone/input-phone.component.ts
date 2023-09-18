13762b00564b2cdc57437447526218b2aa4a9ba5
Xaviermora <xaviermoravega12@gmail.com>
2023-09-18 20:38:29 -0300

    propiedad formControl del input phone

--- src/app/shared/components/input-phone/input-phone.component.ts
+++ src/app/shared/components/input-phone/input-phone.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-input-phone',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./input-phone.component.css']
 })
 export class InputPhoneComponent {
-
+  @Input() control: FormControl = new FormControl()
 }
