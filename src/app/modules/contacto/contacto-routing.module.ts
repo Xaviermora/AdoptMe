bd7d64788becad7899218866e63b4f8f4953d5ea
brashian <bsandovala12@gmail.com>
2023-09-20 08:50:39 -0300

    routing contacto

--- src/app/modules/contacto/contacto-routing.module.ts
+++ src/app/modules/contacto/contacto-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactoComponent } from './pages/contacto/contacto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'contacto',
+    component: ContactoComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
