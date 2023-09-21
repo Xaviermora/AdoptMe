145d37d886afd1da4ca1460e2611d06036700486
Xaviermora <xaviermoravega12@gmail.com>
2023-09-21 18:45:05 -0300

    Atributos severity y msg

--- src/app/shared/components/toast/toast.component.ts
+++ src/app/shared/components/toast/toast.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-toast',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./toast.component.css']
 })
 export class ToastComponent {
-
+  @Input() severity!: string
+  @Input() msg!: string
 }
