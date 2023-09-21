c021fc04fadeb6396ae44ed50549f05f230257ed
Xaviermora <xaviermoravega12@gmail.com>
2023-09-21 18:20:35 -0300

    Creación de componente toast

---
+++ src/app/shared/components/toast/toast.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toast',
+  templateUrl: './toast.component.html',
+  styleUrls: ['./toast.component.css']
+})
+export class ToastComponent {
+
+}
