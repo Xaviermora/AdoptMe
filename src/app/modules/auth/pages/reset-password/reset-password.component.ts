7d490b14bf0e2193332420a3e6bd02aa2584df30
Xaviermora <xaviermoravega12@gmail.com>
2023-09-24 21:32:39 -0300

    Método onsubmit del reset password form

--- src/app/modules/auth/pages/reset-password/reset-password.component.ts
+++ src/app/modules/auth/pages/reset-password/reset-password.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reset-password',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./reset-password.component.css']
 })
 export class ResetPasswordComponent {
+  resetPassword = new FormGroup({
+    email: new FormControl('', Validators.required)
+  })
+  showMsg: boolean = false
+  msgContent!: string
 
+  constructor(private authService: AuthService){}
+
+  onSubmit(){
+    const email = this.resetPassword.value.email
+
+    if(email){
+      this.authService.resetPassword(email)
+      .then(() => {
+        this.msgContent = 'Le enviamos un correo para que cambie su contraseña.'
+        this.showMsg = true
+      })
+      .catch(error => {
+        this.msgContent = this.authService.firebaseErrors(error.code)
+        this.showMsg = true
+      })
+    }
+  }
 }
