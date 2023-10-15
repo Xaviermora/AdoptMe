1e1af43970166e4cfc0e224ff066701021b6f384
Xaviermora <xaviermoravega12@gmail.com>
2023-10-15 02:39:50 -0300

    Atributo link en el item dropdown

--- src/app/shared/components/dropdown-menu/item-dropdown-menu/item-dropdown-menu.component.ts
+++ src/app/shared/components/dropdown-menu/item-dropdown-menu/item-dropdown-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-item-dropdown-menu',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./item-dropdown-menu.component.css']
 })
 export class ItemDropdownMenuComponent {
-
+  @Input() link!: string
 }
