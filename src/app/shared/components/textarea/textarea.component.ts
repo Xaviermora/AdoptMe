c77131cb85545aaa2c6a5f3a7c5b6e61ec427406
brashian <bsandovala12@gmail.com>
2023-10-23 10:50:22 -0300

    interpolacion text-area

--- src/app/shared/components/textarea/textarea.component.ts
+++ src/app/shared/components/textarea/textarea.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-textarea',
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class TextareaComponent {
 
+  @Input() rows!: number;
+
+  @Input() placeholder!: string;
 }
