c1c11fd8bba1d433f7f1b30398a51ff39adde941
Xaviermora <xaviermoravega12@gmail.com>
2023-10-22 23:23:07 -0300

    Se importa reactive forms module

--- src/app/modules/dar-en-adopcion/dar-en-adopcion.module.ts
+++ src/app/modules/dar-en-adopcion/dar-en-adopcion.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { DarEnAdopcionRoutingModule } from './dar-en-adopcion-routing.module';
 import { DarEnAdopcionFormComponent } from './pages/dar-en-adopcion-form/dar-en-adopcion-form.component';
 import { SharedModule } from "../../shared/shared.module";
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
     declarations: [
@@ -13,7 +12,8 @@ import { SharedModule } from "../../shared/shared.module";
     imports: [
         CommonModule,
         DarEnAdopcionRoutingModule,
-        SharedModule
+        SharedModule,
+        ReactiveFormsModule
     ]
 })
 export class DarEnAdopcionModule { }
