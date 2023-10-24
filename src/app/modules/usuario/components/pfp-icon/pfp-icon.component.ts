3749cedb18731dcf35bf453478d2fe4abbeeb126
brashian <bsandovala12@gmail.com>
2023-10-24 10:22:21 -0300

    interpolacion icon-pfp

--- src/app/modules/usuario/components/pfp-icon/pfp-icon.component.ts
+++ src/app/modules/usuario/components/pfp-icon/pfp-icon.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
 
 @Component({
   selector: 'app-pfp-icon',
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./pfp-icon.component.css']
 })
 export class PfpIconComponent {
-
+  @Input() userIcon!: string;
+  @Input() edad!: number;
+  @Input() email!: string;
+  @Input() usuario!: string;
 }
