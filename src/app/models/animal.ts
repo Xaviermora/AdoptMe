c18f3687034f503f91e90952b128d49116b8bca2
Xaviermora <xaviermoravega12@gmail.com>
2023-10-24 10:32:28 -0300

    Interface animal

---
+++ src/app/models/animal.ts
@@ -0,0 +1,15 @@
+export interface Animal {
+  id: string,
+  animal: string,
+  nombre: string,
+  edad: string,
+  sexo: string,
+  castrado: boolean,
+  raza: string,
+  provincia: string,
+  ciudad: string,
+  imgs: string[],
+  descripcion: string,
+  requisitos: string
+  userId: string
+}
