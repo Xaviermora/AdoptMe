4ba6f0a0a1041fb83e447d393c1f234a8a2b48d3
Xaviermora <xaviermoravega12@gmail.com>
2023-10-26 19:29:24 -0300

    Lógica para obtener las provincias y ciudades a traves del servicio

--- src/app/modules/usuario/pages/config-datos-personales/config-datos-personales/config-datos-personales.component.ts
+++ src/app/modules/usuario/pages/config-datos-personales/config-datos-personales/config-datos-personales.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ProvinciasCiudadesService } from 'src/app/shared/services/provincias-ciudades.service';
 
 @Component({
   selector: 'app-config-datos-personales',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./config-datos-personales.component.css']
 })
 export class ConfigDatosPersonalesComponent {
+  provincias!: string[]
+  ciudades!: string[]
 
+  datosPersonalesUpdate = new FormGroup({
+    provincia: new FormControl(''),
+    ciudad: new FormControl({value: '', disabled: true}, Validators.required)
+  })
+
+  constructor(private provinciasCiudadesService: ProvinciasCiudadesService){
+    this.provincias = provinciasCiudadesService.getProvincias()
+    
+    this.provinciasCiudadesService.getCiudades(this.datosPersonalesUpdate).subscribe(ciudades => this.ciudades = ciudades)
+  } 
 }
--- src/app/shared/services/provincias-ciudades.service.ts
+++ src/app/shared/services/provincias-ciudades.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -24,11 +26,81 @@ export class ProvinciasCiudadesService {
     {
       provincia: 'Neuquen',
       ciudades: [
-        "Cipolleti",
-        "General Fernandez Oro",
+        "Neuquén",
+        "Plottier",
+        "Cutral-Có",
+        "Plaza Huincul",
+        "Centenario",
+        "Zapala",
+        "San Martín de los Andes",
+        "Junín de los Andes",
+        "Villa La Angostura",
+        "Rincón de los Sauces",
+        "Chos Malal",
+        "Las Lajas",
+        "Aluminé",
+        "Loncopué",
+        "Añelo"
+      ]
+    },
+    {
+      provincia: 'Chubut',
+      ciudades: [
+        "Rawson",
+        "Trelew",
+        "Comodoro Rivadavia",
+        "Puerto Madryn",
+        "Esquel",
+        "Río Gallegos",
+        "Gaiman",
+        "Dolavon",
+        "Puerto Pirámides",
+        "Sarmiento",
+        "Trevelin",
+        "Paso de Indios",
+        "Tecka",
+        "Río Mayo",
+        "El Hoyo"
+      ]
+    },
+    {
+      provincia: 'Santa Cruz',
+      ciudades: [
+        "Río Gallegos",
+        "El Calafate",
+        "Caleta Olivia",
+        "Pico Truncado",
+        "Las Heras",
+        "Puerto San Julián",
+        "Comandante Luis Piedrabuena",
+        "Puerto Santa Cruz",
+        "28 de Noviembre",
+        "Gobernador Gregores",
+        "Río Turbio",
+        "Perito Moreno",
+        "Los Antiguos",
+        "Puerto Deseado",
+        "Tellier"
       ]
     }
   ]
 
   constructor() { }
+
+  getProvincias(){
+    return this.ubicaciones.map(ubicacion => ubicacion.provincia)
+  }
+
+  getCiudades(form: FormGroup){
+    let ciudades: Subject<string[]> = new Subject()
+
+    form.controls['provincia'].valueChanges.subscribe(provincia => { // Se subscribe al observable de cuando el valor de la provincia cambia
+      if (form.controls['ciudad'].disabled) form.controls['ciudad'].enable() // Se habilita el select de ciudad si no lo está
+      this.ubicaciones
+        .filter(ubicacion => ubicacion.provincia === provincia) // Se filtra a las ciudades por la provincia seleccionada
+        .map(provincia => ciudades.next(provincia.ciudades)) // Se devuelve el array de las ciudades de la provincia seleccionada
+    })
+
+    return ciudades.asObservable()
+  }
 }
