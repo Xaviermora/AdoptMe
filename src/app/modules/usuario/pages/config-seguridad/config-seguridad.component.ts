a8efa42de6e23ef1d402e9a9721a1ad17812b716
Xaviermora <xaviermoravega12@gmail.com>
2023-10-29 17:09:56 -0300

    Modal eliminar cuenta

--- src/app/modules/usuario/pages/config-seguridad/config-seguridad.component.ts
+++ src/app/modules/usuario/pages/config-seguridad/config-seguridad.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { Modal } from 'flowbite';
 
 @Component({
   selector: 'app-config-seguridad',
   templateUrl: './config-seguridad.component.html',
   styleUrls: ['./config-seguridad.component.css']
 })
-export class ConfigSeguridadComponent {
+export class ConfigSeguridadComponent implements AfterViewInit{
+  modal!: Modal
+  constructor(){}
 
+  ngAfterViewInit(){
+    const $targetEl = document.getElementById('modalEliminarCuenta')
+    this.modal = new Modal($targetEl)
+  }
 }
