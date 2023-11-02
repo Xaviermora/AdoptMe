31651d8044e8f9b2fabfbfc01e050eb49bb3d31b
Xaviermora <xaviermoravega12@gmail.com>
2023-11-02 00:50:04 -0300

    Solución de error en la filter pipe

--- src/app/shared/pipes/filter.pipe.ts
+++ src/app/shared/pipes/filter.pipe.ts
@@ -7,6 +7,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FilterPipe implements PipeTransform {
   transform(list: string[], searchValue: string = ''): string[]{
     return list.map(item => item.toLowerCase()) // Devuelve un nuevo array con todos los items en minúscula
-              .filter(item => item.includes(searchValue.toLowerCase())) // Devuelve los items que contengan algun valor de la busqueda
+              .filter(item => item.includes(searchValue?.toLowerCase())) // Devuelve los items que contengan algun valor de la busqueda
   }
 }
