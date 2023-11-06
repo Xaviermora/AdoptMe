d064895e3d85285b5f793c1ddb6adeb08ae976dc
Xaviermora <xaviermoravega12@gmail.com>
2023-11-06 10:35:52 -0300

    Atributo animal id

--- src/app/modules/animales/components/card/modal-reportar-publicacion/modal-reportar-publicacion.component.ts
+++ src/app/modules/animales/components/card/modal-reportar-publicacion/modal-reportar-publicacion.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-modal-reportar-publicacion',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./modal-reportar-publicacion.component.css']
 })
 export class ModalReportarPublicacionComponent {
-
+  @Input() animalId!: string
 }
