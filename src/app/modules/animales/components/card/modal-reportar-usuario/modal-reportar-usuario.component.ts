1f529432931e791558a5630ad40e26d0ec8b4fbe
Xaviermora <xaviermoravega12@gmail.com>
2023-11-06 10:49:52 -0300

    Atributo usuario id

--- src/app/modules/animales/components/card/modal-reportar-usuario/modal-reportar-usuario.component.ts
+++ src/app/modules/animales/components/card/modal-reportar-usuario/modal-reportar-usuario.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-modal-reportar-usuario',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./modal-reportar-usuario.component.css']
 })
 export class ModalReportarUsuarioComponent {
-
+  @Input() usuarioId!: string
 }
