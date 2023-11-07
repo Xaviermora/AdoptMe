8b6946d26ca2ecc6e838cb3e70a5b56ae5d6dcbc
Xaviermora <xaviermoravega12@gmail.com>
2023-11-07 09:33:01 -0300

    Formulario para editar el perfil publico

--- src/app/modules/usuario/pages/config-perfil-publico/config-perfil-publico.component.ts
+++ src/app/modules/usuario/pages/config-perfil-publico/config-perfil-publico.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Usuario } from 'src/app/models/usuario';
 
 @Component({
   selector: 'app-config-perfil-publico',
@@ -6,6 +8,25 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./config-perfil-publico.component.css']
 })
 export class ConfigPerfilPublicoComponent {
-  @Input() usuario!: any
+  @Input() usuario!: Usuario
 
+  perfilPublicoUpdate = new FormGroup({
+    photoURL: new FormControl('', Validators.required),
+    nombre: new FormControl('', Validators.required),
+    apellido: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    fechaDeNacimiento: new FormControl<any>('', Validators.required)
+  })
+
+  ngOnInit(){
+    if (this.usuario){
+      this.perfilPublicoUpdate.setValue({
+        photoURL: this.usuario.photoURL,
+        nombre: this.usuario.nombre,
+        apellido: this.usuario.apellido,
+        email: this.usuario.email,
+        fechaDeNacimiento: this.usuario.fechaDeNacimiento
+      })
+    }
+  }
 }
