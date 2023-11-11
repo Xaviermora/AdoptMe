c3452d6c3b1b54598a5e1e569e7d277ee6c4a5c6
Xaviermora <xaviermoravega12@gmail.com>
2023-11-11 19:50:29 -0300

    Motivos de reportar publicación

--- src/app/modules/animales/components/card/modal-reportar-publicacion/modal-reportar-publicacion.component.ts
+++ src/app/modules/animales/components/card/modal-reportar-publicacion/modal-reportar-publicacion.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-modal-reportar-publicacion',
@@ -7,4 +8,33 @@ import { Component, Input } from '@angular/core';
 })
 export class ModalReportarPublicacionComponent {
   @Input() animalId!: string
+
+  motivosReporte: string[] = [
+    'Información falsa',
+    'Desnudos o actividad sexual',
+    'Violencia',
+    'Estafa o fraude',
+    'Es spam',
+    'Lenguaje o simbolos que incitan al odio',
+    'Otro motivo'
+  ]
+
+  reportarPublicacion!: FormGroup
+
+  ngOnInit(){
+    this.reportarPublicacion = new FormGroup({})
+
+    this.motivosReporte.forEach(motivo => {
+      this.reportarPublicacion.addControl(motivo, new FormControl(false))
+    })
+  }
+
+  getControl(control: string){
+    return this.reportarPublicacion.get(control) as FormControl
+  }
+
+  toggleCheckbox(control: string){
+    let checkoxControl = this.getControl(control) as FormControl
+    checkoxControl.setValue(!checkoxControl.value)
+  }
 }
