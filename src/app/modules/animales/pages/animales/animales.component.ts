38a100d8222eaa96471e64a95ba1da844f2ea3f6
Xaviermora <xaviermoravega12@gmail.com>
2023-11-12 17:49:49 -0300

    Se elimina array de publicaciones innecesario

--- src/app/modules/animales/pages/animales/animales.component.ts
+++ src/app/modules/animales/pages/animales/animales.component.ts
@@ -8,34 +8,6 @@ import { Animal } from 'src/app/models/animal';
   styleUrls: ['./animales.component.css']
 })
 export class AnimalesComponent {
-  publicaciones = [
-    {
-      id: 0,
-      nombre: 'Figaro',
-      imagenesAnimal: [
-        '../../../../../assets/michi.jpeg',
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Cat_on_its_back.jpg/220px-Cat_on_its_back.jpg',
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Gato_Siam%C3%A9s_ojos_azules.JPG/220px-Gato_Siam%C3%A9s_ojos_azules.JPG'
-      ]
-    },
-    {
-      id: 1,
-      nombre: 'Figaro2',
-      imagenesAnimal: [
-        '../../../../../assets/michi.jpeg',
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Cat_on_its_back.jpg/220px-Cat_on_its_back.jpg',
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Gato_Siam%C3%A9s_ojos_azules.JPG/220px-Gato_Siam%C3%A9s_ojos_azules.JPG'
-      ]
-    },
-    {
-      id: 2,
-      nombre: 'Carlitos',
-      imagenesAnimal: [
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Cat_on_its_back.jpg/220px-Cat_on_its_back.jpg',
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Gato_Siam%C3%A9s_ojos_azules.JPG/220px-Gato_Siam%C3%A9s_ojos_azules.JPG'
-      ]
-    }
-  ]
   animales: Animal[] = []
   filtros!: any
   loadingAnimales: boolean = true
