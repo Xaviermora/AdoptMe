9321f91a7027b1fe8514c7d1423f5fe7b5ccae8a
GonzaloLatorre <gonza.latorre21@gmail.com>
2023-11-14 08:49:20 -0300

    hice la ruta de reportes

--- src/app/modules/admin/admin-routing.module.ts
+++ src/app/modules/admin/admin-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './pages/admin/admin.component';
+import { ReportesComponent } from './pages/reportes/reportes.component';
 
 const routes: Routes = [
   {
     path: 'admin' ,
     component: AdminComponent,
+  },
+  {
+    path: 'reportes',
+    component: ReportesComponent,
   }
 ];
 
