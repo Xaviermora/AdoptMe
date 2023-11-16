77f203e3d19680ab07415b6e128d68182d936a6e
Xaviermora <xaviermoravega12@gmail.com>
2023-11-16 14:13:50 -0300

    Métodos para ver si el usuario existe en la colección de usuarios

--- src/app/shared/guards/auth.guard.ts
+++ src/app/shared/guards/auth.guard.ts
@@ -1,6 +1,7 @@
 import { inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/modules/auth/services/auth.service';
+import { UsuariosService } from '../services/usuarios.service';
 
 export const userInSession = () => {
   const authService = inject(AuthService)
@@ -15,3 +16,27 @@ export const userNotInSession = () => {
 
   return authService.user.subscribe(user => !user ? true : router.navigate(['/']))
 };
+
+export const userNotExistsInCollection = () => {
+  const authService = inject(AuthService)
+  const router = inject(Router)
+  const usuariosService = inject(UsuariosService)
+
+  return new Promise(resolve => {
+    authService.user.subscribe(user => {
+      usuariosService.getUser(user!.uid).subscribe(user => user ? router.navigate(['/']) : resolve(true))
+    })
+  })
+}
+
+export const userExistsInCollection = () => {
+  const authService = inject(AuthService)
+  const router = inject(Router)
+  const usuariosService = inject(UsuariosService)
+
+  return new Promise((resolve) => {
+    authService.user.subscribe(user => {
+      usuariosService.getUser(user!.uid).subscribe(user => user ? resolve(true) : router.navigate(['/datos-personales']))
+    })
+  })
+}
