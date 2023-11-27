1975aa8703e4743789f150eeafbb4a70180b3fb4
Xaviermora <xaviermoravega12@gmail.com>
2023-11-27 10:57:06 -0300

    Se hace uso del metodo para crear una notificacion

--- src/app/modules/animales/components/card/modal/modal.component.ts
+++ src/app/modules/animales/components/card/modal/modal.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 import { Modal } from 'flowbite';
 import { Animal } from 'src/app/models/animal';
+import { AuthService } from 'src/app/modules/auth/services/auth.service';
+import { NotificacionesService } from 'src/app/modules/notificaciones/services/notificaciones.service';
 
 @Component({
   selector: 'app-modal',
@@ -10,4 +13,17 @@ import { Animal } from 'src/app/models/animal';
 export class ModalComponent {
   @Input() animal!: Animal
   @Input() modal!: Modal
+
+  constructor(private notificacionesService: NotificacionesService, private authService: AuthService, private router: Router){}
+
+  solicitarAdopcion(){
+    this.authService.user.subscribe(user => {
+      if(user){
+        this.notificacionesService.createNotificacion({
+          idAnimal: this.animal.id,
+          idUsuarioAdoptante: user.uid
+        })
+      }else{this.router.navigate(['/login'])}
+    })
+  }
 }
