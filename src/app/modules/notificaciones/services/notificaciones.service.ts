b0d9cfdc4c0f905cc74ec6ef523ef1bbdc5d4b80
Xaviermora <xaviermoravega12@gmail.com>
2023-11-27 10:47:48 -0300

    Metodo para crear notificacion

--- src/app/modules/notificaciones/services/notificaciones.service.ts
+++ src/app/modules/notificaciones/services/notificaciones.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { Notificacion } from 'src/app/models/notificacion';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificacionesService {
+  private notificacionesCollection: AngularFirestoreCollection<Notificacion>
 
-  constructor() { }
+  constructor(private database: AngularFirestore) {
+    this.notificacionesCollection = this.database.collection<Notificacion>('notificaciones')
+  }
+
+  async createNotificacion(data: any){
+    const notificacion = {
+      id: this.database.createId(),
+      ...data
+    }
+
+    await this.notificacionesCollection.doc(notificacion.id).set(notificacion)
+  }
 }
