28f65580a529eb99dba362b87ba57a792c37505b
Xaviermora <xaviermoravega12@gmail.com>
2023-11-27 11:15:00 -0300

    Creación de page notificaciones y componente notificacion item

---
+++ src/app/modules/notificaciones/components/notificacion-item/notificacion-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notificacion-item',
+  templateUrl: './notificacion-item.component.html',
+  styleUrls: ['./notificacion-item.component.css']
+})
+export class NotificacionItemComponent {
+
+}
--- src/app/modules/notificaciones/notificaciones.module.ts
+++ src/app/modules/notificaciones/notificaciones.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { NotificacionesRoutingModule } from './notificaciones-routing.module';
+import { NotificacionItemComponent } from './components/notificacion-item/notificacion-item.component';
+import { NotificacionesComponent } from './pages/notificaciones/notificaciones.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NotificacionItemComponent,
+    NotificacionesComponent
+  ],
   imports: [
     CommonModule,
     NotificacionesRoutingModule
