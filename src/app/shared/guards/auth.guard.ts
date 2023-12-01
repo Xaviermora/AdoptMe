672c0763390436a827bc41a329dc82a126613525
Xaviermora <xaviermoravega12@gmail.com>
2023-12-01 15:16:13 -0300

    Se soluciona error de funcionalidad de los guards usando pipe y map

--- src/app/shared/guards/auth.guard.ts
+++ src/app/shared/guards/auth.guard.ts
@@ -2,23 +2,38 @@ import { inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/modules/auth/services/auth.service';
 import { UsuariosService } from '../services/usuarios.service';
+import { map } from 'rxjs';
 
 export const userInSession = () => {
   const authService = inject(AuthService)
   const router = inject(Router)
 
-  return new Promise(resolve => {
-    authService.user.subscribe(user => user ? resolve(true) : router.createUrlTree(['/login']))
-  })
+  return authService.user.pipe(
+    map(user => {
+      if (user) {
+        return true; // Permite el acceso si el usuario no está en sesión
+      } else {
+        // Redirige a la página principal si el usuario ya está en sesión
+        return router.navigate(['/login']);
+      }
+    })
+  );
 };
 
 export const userNotInSession = () => {
   const authService = inject(AuthService)
   const router = inject(Router)
 
-  return new Promise<boolean>(resolve => {
-    authService.user.subscribe(user => !user ? resolve(true) : router.createUrlTree(['/']))
-  })
+  return authService.user.pipe(
+    map(user => {
+      if (!user) {
+        return true; // Permite el acceso si el usuario no está en sesión
+      } else {
+        // Redirige a la página principal si el usuario ya está en sesión
+        return router.navigate(['/']);
+      }
+    })
+  );
 };
 
 export const userNotExistsInCollection = () => {
