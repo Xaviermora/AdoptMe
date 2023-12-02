4aabf9f9f52edb9bbf5fca8da574446414c72a26
brashian <bsandovala12@gmail.com>
2023-12-02 04:26:40 -0300

    comentarios

--- src/app/modules/auth/pages/reset-password/reset-password.component.ts
+++ src/app/modules/auth/pages/reset-password/reset-password.component.ts
@@ -9,23 +9,30 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class ResetPasswordComponent {
   resetPassword = new FormGroup({
+    //FormGroup con solo el control de email para la contraseña
     email: new FormControl('', Validators.required)
   })
-  severityToast!: string
-  showMsg: boolean = false
-  msgContent!: string
-  resetPasswordIsSubmitted: boolean = false
-  loading: boolean = false
+  severityToast!: string //error succes, etc
+  showMsg: boolean = false // Determina si mostrar el Toast
+  msgContent!: string // Contenido del mensaje del toast
+
+  resetPasswordIsSubmitted: boolean = false // Bandera de envio de formulario
+  loading: boolean = false //bandera de carga mientras procesa
 
   constructor(private authService: AuthService){}
 
   onSubmit(){
+    //obtiene unicamente el valor de  email del formulario resetPassword
     const email = this.resetPassword.value.email
+    //Marca al formulario como enviado
     this.resetPasswordIsSubmitted = true
 
     if(email){
       this.loading = true
+      //Reset de contraseña con email mediante el authService
       this.authService.resetPassword(email)
+
+      //Mostrar mensajes de exito o error
       .then(() => {
         this.severityToast = 'info'
         this.msgContent = 'Le enviamos un correo para que cambie su contraseña.'
